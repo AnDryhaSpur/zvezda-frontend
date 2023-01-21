a0f9b78cf561159e17209c22e830ca27896b4e89
Dmitrii Gherastovschi <dgherastovschi@icloud.com>
2023-01-21 18:27:52 +0500

    fixed navbar, slides styling basic padding

--- dmitrii/scripts/main.js
+++ dmitrii/scripts/main.js
@@ -7,7 +7,7 @@ const swiper = new Swiper('.swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
