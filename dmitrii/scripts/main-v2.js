549d33be8301b0f2b0e6bb8c5abb902860de52b0
Dmitrii Gherastovschi <dgherastovschi@icloud.com>
2023-02-05 21:01:47 +0500

    styling of the rest of the page. news pictures

--- dmitrii/scripts/main-v2.js
+++ dmitrii/scripts/main-v2.js
@@ -1,6 +1,28 @@
-var swiper = new Swiper(".news", {
+var hero = new Swiper(".hero", {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  centeredSlides: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var news = new Swiper(".newsSwiper", {
   slidesPerView: 3,
-  spaceBetween: 30,
+  spaceBetween: 20,
+});
+
+var catalogSlider = new Swiper(".catalogSlider", {
+  slidesPerView: 4,
+  grid: {
+    columns: 2,
+  },
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
