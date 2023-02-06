3f55f2834f27619f0c51f949d00745b46ba9a45a
Dmitrii Gherastovschi <dgherastovschi@icloud.com>
2023-02-06 12:51:00 +0500

    final push before demo

--- dmitrii/scripts/main-v2.js
+++ dmitrii/scripts/main-v2.js
@@ -10,11 +10,18 @@ var hero = new Swiper(".hero", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  autoplay: {
+    delay: 5000,
+  },
 });
 
 var news = new Swiper(".newsSwiper", {
   slidesPerView: 3,
   spaceBetween: 20,
+
+  autoplay: {
+    delay: 5000,
+  },
 });
 
 var catalogSlider = new Swiper(".catalogSlider", {
@@ -28,3 +35,23 @@ var catalogSlider = new Swiper(".catalogSlider", {
     clickable: true,
   },
 });
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
