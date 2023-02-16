b627e3069ec5623cbcf05789eef71d3a3d95938d
Dmitrii Gherastovschi <dgherastovschi@icloud.com>
2023-02-16 16:19:08 +0500

    imported the project

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+// let urlInput = document.querySelector("input#url");
+// let button = document.querySelector(".button");
+// let tint = document.querySelector(".tint");
+// let colorId = window.localStorage.getItem("color") || "red";
+// console.log(colorId);
+// let radio = document.querySelector(`#${colorId}`);
+// tint.style.backgroundColor = radio.id;
+
+// let image = document.querySelector("img");
+// let getImageUrl = window.localStorage.getItem("url") || image.src;
+// image.src = getImageUrl;
+
+// button.addEventListener("click", () => {
+//   radio = document.querySelector('input[name="color-selector"]:checked');
+//   tint.style.backgroundColor = radio.id;
+//   localStorage.setItem("color", radio.id);
+//   image.src = urlInput.value;
+//   localStorage.setItem("url", urlInput.value);
+//   urlInput.value = "";
+// });
