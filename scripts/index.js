43ec2b843fdb0caeae7250c6e479fe973d7d0d33
Andrew <spuryaas@gmail.com>
2023-03-24 13:44:27 +0500

    Heading and catalog changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
+let slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [1000, 5000],
+    step: 100,
+    connect: true,
+    range: {
+        'min': 1000,
+        'max': 5000,
+    }
+});
+
+function upd() {
+    slider.dispatchEvent(new Event('sliderupdate'))
+}
+slider.noUiSlider.on('update', upd);
+
+
 const heroSwiper = new Swiper(".hero .swiper", {
     // Optional parameters
     loop: true,
@@ -12,6 +30,28 @@ const heroSwiper = new Swiper(".hero .swiper", {
     pagination: {
         el: ".swiper-pagination",
         type: "fraction"
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
+
+const catalogSwiper = new Swiper(".catalog .swiper", {
+    // Optional parameters
+    speed: 500,
+    slidesPerView: 4,
+    spaceBetween: 16,
+    grid: {
+        fill: 'row',
+        rows: 2,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
     },
 
     // Navigation arrows
